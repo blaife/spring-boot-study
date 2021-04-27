0eb97c5d68d18dd4a855c3871dd6db46d978bb88
blaife <2767026270@qq.com>
2021-04-27 22:45:22 +0800

    :tada:添加信息获取实例-中央处理器信息

---
+++ demo-monitoring-oshi/src/main/java/com/blaife/oshi/entity/whole/CpuInfo.java
@@ -0,0 +1,64 @@
+package com.blaife.oshi.entity.whole;
+
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.CentralProcessor.TickType;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.FormatUtil;
+import oshi.util.Util;
+
+import java.util.Arrays;
+
+/**
+ * @author blaife
+ * @description 中央处理器信息
+ * @data 2021/4/27 22:17
+ */
+public class CpuInfo {
+
+    public static void main(String[] args) {
+        // 获取系统对象（系统对象中包含很多内容）
+        SystemInfo si = new SystemInfo();
+        // 获取硬件抽象层对象
+        HardwareAbstractionLayer hal = si.getHardware();
+        // 获取处理器信息
+        CentralProcessor processor = hal.getProcessor();
+
+        System.out.println("正常运行时间: " + FormatUtil.formatElapsedSecs(processor.getSystemUptime()));
+        System.out.println("上下文切换/中断: " + processor.getContextSwitches() + " / " + processor.getInterrupts());
+
+        long[] prevTicks = processor.getSystemCpuLoadTicks();
+        System.out.println("CPU、IO等待、中断请求 [0秒记号]: " + Arrays.toString(prevTicks));
+        // 稍等一下。。。
+        Util.sleep(1000);
+        long[] ticks = processor.getSystemCpuLoadTicks();
+        System.out.println("CPU、IO等待、中断请求 [1秒记号]:" + Arrays.toString(ticks));
+        long user = ticks[TickType.USER.getIndex()] - prevTicks[TickType.USER.getIndex()];
+        long nice = ticks[TickType.NICE.getIndex()] - prevTicks[TickType.NICE.getIndex()];
+        long sys = ticks[TickType.SYSTEM.getIndex()] - prevTicks[TickType.SYSTEM.getIndex()];
+        long idle = ticks[TickType.IDLE.getIndex()] - prevTicks[TickType.IDLE.getIndex()];
+        long iowait = ticks[TickType.IOWAIT.getIndex()] - prevTicks[TickType.IOWAIT.getIndex()];
+        long irq = ticks[TickType.IRQ.getIndex()] - prevTicks[TickType.IRQ.getIndex()];
+        long softirq = ticks[TickType.SOFTIRQ.getIndex()] - prevTicks[TickType.SOFTIRQ.getIndex()];
+        long steal = ticks[TickType.STEAL.getIndex()] - prevTicks[TickType.STEAL.getIndex()];
+        long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
+
+        System.out.format( "User: %.1f%% Nice: %.1f%% System: %.1f%% Idle: %.1f%% IOwait: %.1f%% IRQ: %.1f%% SoftIRQ: %.1f%% Steal: %.1f%%%n",
+                100d * user / totalCpu, 100d * nice / totalCpu, 100d * sys / totalCpu, 100d * idle / totalCpu,
+                100d * iowait / totalCpu, 100d * irq / totalCpu, 100d * softirq / totalCpu, 100d * steal / totalCpu);
+        System.out.format("CPU 负载: %.1f%% (counting ticks)%n", processor.getSystemCpuLoadBetweenTicks() * 100);
+        System.out.format("CPU 负载: %.1f%% (OS MXBean)%n", processor.getSystemCpuLoad() * 100);
+        double[] loadAverage = processor.getSystemLoadAverage(3);
+        System.out.println("CPU 平均负载:" + (loadAverage[0] < 0 ? " N/A" : String.format(" %.2f", loadAverage[0]))
+                + (loadAverage[1] < 0 ? " N/A" : String.format(" %.2f", loadAverage[1]))
+                + (loadAverage[2] < 0 ? " N/A" : String.format(" %.2f", loadAverage[2])));
+        // 每个核心CPU
+        StringBuilder procCpu = new StringBuilder("每个处理器CPU负载:");
+        double[] load = processor.getProcessorCpuLoadBetweenTicks();
+        for (double avg : load) {
+            procCpu.append(String.format(" %.1f%%", avg * 100));
+        }
+        System.out.println(procCpu.toString());
+    }
+
+}
