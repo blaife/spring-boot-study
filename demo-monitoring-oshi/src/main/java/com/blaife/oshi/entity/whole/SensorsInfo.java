9e876f3e8d6b33ae464cf4e7519b4902653c7724
blaife <2767026270@qq.com>
2021-04-30 09:06:21 +0800

    :tada:添加信息获取实例-传感器信息

--- demo-monitoring-oshi/src/main/java/com/blaife/oshi/entity/whole/SensorsInfo.java
+++ demo-monitoring-oshi/src/main/java/com/blaife/oshi/entity/whole/SensorsInfo.java
@@ -20,7 +20,6 @@ public class SensorsInfo {
         HardwareAbstractionLayer hal = si.getHardware();
         // 获取传感器信息
         Sensors sensors = hal.getSensors();
-        System.out.println(sensors);
         System.out.println("CPU 温度: " + sensors.getCpuTemperature());
         System.out.println("CPU 电压: " + sensors.getCpuVoltage() + "V");
         System.out.println("风扇转速: " + Arrays.toString(sensors.getFanSpeeds()));
