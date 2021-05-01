700ebd816f9b771b339c578aae17667a3ba35cb0
blaife <2767026270@qq.com>
2021-05-01 17:21:26 +0800

    :tada:添加信息获取实例-显示器信息

---
+++ demo-monitoring-oshi/src/main/java/com/blaife/oshi/entity/whole/DisplaysInfo.java
@@ -0,0 +1,29 @@
+package com.blaife.oshi.entity.whole;
+
+import oshi.SystemInfo;
+import oshi.hardware.Display;
+import oshi.hardware.HardwareAbstractionLayer;
+
+
+/**
+ * @author blaife
+ * @description 显示器信息
+ * @data 2021/5/1 17:12
+ */
+public class DisplaysInfo {
+
+    public static void main(String[] args) {
+        // 获取系统对象（系统对象中包含很多内容）
+        SystemInfo si = new SystemInfo();
+        // 获取硬件抽象层对象
+        HardwareAbstractionLayer hal = si.getHardware();
+        // 显示器信息
+        Display[] displays = hal.getDisplays();
+
+        for (Display display : displays) {
+            System.out.println("------------------------------------------------------");
+            System.out.println(display.toString());
+        }
+    }
+
+}
