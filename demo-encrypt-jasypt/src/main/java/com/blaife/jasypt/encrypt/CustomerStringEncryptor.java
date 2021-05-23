0a6b27117e9b53a1d2fcb3d33b32fcec9bee3eee
blaife <2767026270@qq.com>
2021-05-23 14:36:56 +0800

    :sparkles: Jasypt 自定义加密尝试

---
+++ demo-encrypt-jasypt/src/main/java/com/blaife/jasypt/encrypt/CustomerStringEncryptor.java
@@ -0,0 +1,20 @@
+package com.blaife.jasypt.encrypt;
+
+import org.jasypt.encryption.StringEncryptor;
+
+/**
+ * @author blaife
+ * @description 自定义加密
+ * @data 2021/5/14 10:33
+ */
+public class CustomerStringEncryptor implements StringEncryptor {
+    @Override
+    public String encrypt(String s) {
+        return null;
+    }
+
+    @Override
+    public String decrypt(String s) {
+        return null;
+    }
+}
