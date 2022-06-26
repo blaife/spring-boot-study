281ea4dd60686908a82bcdea37adf056b526329e
blaife <2767026270@qq.com>
2022-06-26 23:21:50 +0800

    file - EasyExcel 填充案例提交

---
+++ file-easyexcel/src/main/java/com/blaife/easyexcel/FileEasyexcelApplication.java
@@ -0,0 +1,13 @@
+package com.blaife.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileEasyexcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileEasyexcelApplication.class, args);
+    }
+
+}
---
+++ file-easyexcel/src/main/java/com/blaife/easyexcel/entity/FillExcelInfo.java
@@ -0,0 +1,26 @@
+package com.blaife.easyexcel.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class FillExcelInfo {
+
+    private String field1;
+    private int field2;
+    private double field3;
+    private BigDecimal field4;
+    private Date field5;
+    private String field6;
+    private String field7;
+    private String field8;
+    private String field9;
+
+
+}
